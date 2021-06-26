0a7076e6f022053d86152d23426b259451129c9d
Zeal Shah <zealshah96@gmail.com>
2021-06-25 23:18:30 -0400

    commit changes of removing sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
     "gatsby-transformer-remark",
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
