c61c32af492f8732317865363e62e81b82dca5e8
Zeal Shah <zealshah96@gmail.com>
2021-11-13 14:03:48 -0500

    Committing code for server run

---
+++ public/cb1608f2-8547e2cc86913ddad7eb.js
@@ -0,0 +1,3 @@
+/*! For license information please see cb1608f2-8547e2cc86913ddad7eb.js.LICENSE.txt */
+(self.webpackChunkcustom_print=self.webpackChunkcustom_print||[]).push([[523],{8014:function(c,n,e){"use strict";e.d(n,{gc2:function(){return t}});var t={prefix:"fas",iconName:"angle-down",icon:[320,512,[],"f107","M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"]}}}]);
+//# sourceMappingURL=cb1608f2-8547e2cc86913ddad7eb.js.map
