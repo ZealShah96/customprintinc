cdf64bebd12ead5ce789062007b1610271f05791
Zeal Shah <zealshah96@gmail.com>
2021-11-15 14:31:09 -0500

    make it better

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname,'index.html'));
 });
 app.listen(port, () =>
   console.log(`Server is running on: <a target="_blank" rel="noopener" href="http://localhost:${port}/">http://localhost:${port}</a>`));
