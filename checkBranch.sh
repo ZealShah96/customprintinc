e348bb6f4a64899ed317b1725bb2c94d977764a0
Zeal Shah <zealshah96@gmail.com>
2021-11-15 21:28:49 -0500

    commit changes of footer

--- checkBranch.sh
+++ checkBranch.sh
@@ -4,10 +4,19 @@ if grep -Fqe $BRANCH_NAME << EOF
 `git show-branch --all`
 EOF
 then
-    echo "$BRANCH_NAME exists"
+   echo "$BRANCH_NAME exists"
    git checkout server
    git pull origin server
+   git add .
+   git commit -m "Commit Public folder"
+   git push origin server
+   echo "$BRANCH_NAME exists and public commit"
 else
-   git checkout -b server
    echo "$BRANCH_NAME not exists"
+   git checkout -b server
+   git pull origin server
+   git add .
+   git commit -m "Commit Public folder"
+   git push origin server
+   echo "$BRANCH_NAME not exists and public commit"
 fi
