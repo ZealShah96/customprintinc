2bad19e2219675fcd5793f4165eb3aabfe7ccca2
zealshah96 <zealshah96@gmail.com>
2021-12-16 00:40:12 -0500

    Commit html pages

--- public/component---src-pages-faqs-js-db6764d398419dc19026.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcustom_print=self.webpackChunkcustom_print||[]).push([[295],{3892:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(7294),u=n(8426),c=n(9277),a=n(9626);function l(e){return r.createElement(c.Z,{pageName:"FAQs"},r.createElement(a.Z,{header:"FAQs"}),r.createElement(a.Z,null,r.createElement(u.Z,null)))}}}]);
-//# sourceMappingURL=component---src-pages-faqs-js-db6764d398419dc19026.js.map
