5fd17723397b642838d481ab76a63f734ee96493
zealshah96 <zealshah96@gmail.com>
2021-12-19 20:37:22 -0500

    Commit New Wbesite

---
+++ public/component---src-pages-faqs-js-f6a54865f614c0a35261.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcustom_print=self.webpackChunkcustom_print||[]).push([[295],{3892:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(7294),u=n(5561),c=n(3330),a=n(9626);function l(e){return r.createElement(c.Z,{pageName:"FAQs"},r.createElement(a.Z,{header:"FAQs"}),r.createElement(a.Z,null,r.createElement(u.Z,null)))}}}]);
+//# sourceMappingURL=component---src-pages-faqs-js-f6a54865f614c0a35261.js.map
